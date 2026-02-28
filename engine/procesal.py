f785b140240d369a300c410c107d7b1071fed3cc
Luish1276 <notificaciones@bvalegal.com>
2026-02-28 13:08:31 -0600

    Add initial implementation of procesal analysis functions

---
+++ engine/procesal.py
@@ -0,0 +1,20 @@
+# engine/procesal.py
+# Análisis de Procedimiento y Garantías - Costa Rica
+
+def revisar_nulidades(texto):
+    """
+    Busca vicios procesales según el Art. 178 del Código Procesal Penal de CR.
+    - Falta de defensa técnica.
+    - Prueba espuria (Art. 181 CPP).
+    - Defectos en la fundamentación.
+    """
+    pass
+
+def analizar_medidas_cautelares(texto):
+    """
+    Evalúa los presupuestos del Art. 239 del CPP.
+    - Peligro de fuga.
+    - Peligro de obstaculización.
+    - Reincidencia.
+    """
+    pass
